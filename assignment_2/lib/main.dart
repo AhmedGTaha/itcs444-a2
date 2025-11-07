b0caf220f9ac590f371cd2f5faaa66556555e77d
Ahmed Taha <ahmedtahaghodhbani@gmail.com>
2025-11-07 14:53:42 +0300

    sa

---
+++ assignment_2/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Flutter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple App'),
+        ),
+        body: const Center(
+          child: Text(
+            'Hello, world!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
